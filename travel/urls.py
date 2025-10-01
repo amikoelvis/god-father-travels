75d60e03592ce837f347b45bd551c95745cc9a25
amikoelvis <amikoelvis@gmail.com>
2025-10-01 11:30:41 +0300

    Add root url in urls.py

--- travel/urls.py
+++ travel/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.http import HttpResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),  # <--- our API routes
+    path("api/", include("api.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+    # Root URL
+    path('', lambda request: HttpResponse("Welcome to God Father Travels!")),
 ]
