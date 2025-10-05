b3a0238f5983fe4c7329a2576abbace61b69efdd
amikoelvis <amikoelvis@gmail.com>
2025-10-05 10:36:52 +0300

    Add Render deployment configuration

--- travel/settings.py
+++ travel/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static & media
 # -------------------------------
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
